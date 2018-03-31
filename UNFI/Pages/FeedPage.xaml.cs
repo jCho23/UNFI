02fb0c2b8f48f6ed3e4cc800bf313d72088f32d6
June <junecho.ucb@gmail.com>
2018-03-31 13:36:28 -0400

    added FeedPage

---
+++ UNFI/Pages/FeedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UNFI.Pages
+{
+    public partial class FeedPage : ContentPage
+    {
+        public FeedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
