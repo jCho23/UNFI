3b147937ea5e9a62b4e63129da377d73d454581f
June <junecho.ucb@gmail.com>
2018-03-31 15:37:43 -0400

    Added Using

--- UNFI/ViewModels/FeedViewModel.cs
+++ UNFI/ViewModels/FeedViewModel.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 namespace UNFI.ViewModels
 {
     public class FeedViewModel : BaseViewModel
