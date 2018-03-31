d6acce08c97c0f0461b90e294a0136744865d801
June <junecho.ucb@gmail.com>
2018-03-31 15:55:32 -0400

    FeedDetails CB

--- UNFI/Pages/FeedDetailsPage.xaml.cs
+++ UNFI/Pages/FeedDetailsPage.xaml.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using UNFI.Models;
 using Xamarin.Forms;
 
 namespace UNFI.Pages
 {
     public partial class FeedDetailsPage : ContentPage
     {
-        public FeedDetailsPage()
+        public FeedDetailsPage(PostMessageModel postMessage)
         {
+            var feedDetailsPageViewModel = new FeedDetailsViewModel(postMessage);
+            this.BindingContext = feedDetailsPageViewModel;
+
             InitializeComponent();
         }
+
+        void OnReplyTextLimit(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            if (e.NewTextValue.Length > 23)
+            {
+                replyPostInput.Text = replyPostInput.Text.Remove(23);
+
+            }
+        }
+
+
     }
 }
