f69f253dc27b3e41ce5b203ff29750a17b7aa6f4
June <junecho.ucb@gmail.com>
2018-03-31 15:25:08 -0400

    Added FeedViewCell

---
+++ UNFI/Views/FeedViewCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UNFI.Views
+{
+    public partial class FeedViewCell : ContentPage
+    {
+        public FeedViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
