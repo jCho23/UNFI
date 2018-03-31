c8097337c0f6c81786759b30e0942696476a6b41
June <junecho.ucb@gmail.com>
2018-03-31 15:51:10 -0400

    Added feedViewModel

--- UNFI/Pages/FeedPage.xaml.cs
+++ UNFI/Pages/FeedPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using UNFI.Models;
+using UNFI.ViewModels;
 using Xamarin.Forms;
 
 namespace UNFI.Pages
@@ -9,7 +10,32 @@ namespace UNFI.Pages
     {
         public FeedPage()
         {
+            var feedPageViewModel = new FeedViewModel();
+            this.BindingContext = feedPageViewModel;
+
             InitializeComponent();
         }
+
+        async void OnAddButton_Activated(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new FeedAddPostModalPage());
+        }
+
+
+        async void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var postMessage = e.SelectedItem as PostMessageModel;
+            await Navigation.PushAsync(new FeedDetailsPage(postMessage));
+            listViewFeed.SelectedItem = null;
+        }
+
+        async void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            await ((FeedViewModel)BindingContext)?.ExecuteSearch(e.NewTextValue);
+        }
+
     }
 }
