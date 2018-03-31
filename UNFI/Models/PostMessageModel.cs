5c8be43c6f25e86686d5170e2ff16b671c100bba
June <junecho.ucb@gmail.com>
2018-03-31 15:42:16 -0400

    Added PostMessageModel

--- UNFI/Models/PostMessageModel.cs
+++ UNFI/Models/PostMessageModel.cs
@@ -3,8 +3,11 @@ namespace UNFI.Models
 {
     public class PostMessageModel
     {
-        public PostMessageModel()
-        {
-        }
+        public string Id { get; set; }
+
+        public UserProfileModel PostUser { get; set; }
+        public string PostText { get; set; }
+        public int NumberOfLikes { get; set; }
+        public int NumberOfReplies { get; set; }
     }
 }
