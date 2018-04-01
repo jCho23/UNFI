e5e2567fa63097d625b7790ed7d17a3ecfdde7f7
June <junecho.ucb@gmail.com>
2018-04-01 16:04:14 -0400

    ModalPage Xaml

--- UNFI/obj/Debug/UNFI.Pages.BasePage.FeedAddPostModalPage.xaml.g.cs
+++ UNFI/obj/Debug/UNFI.Pages.BasePage.FeedAddPostModalPage.xaml.g.cs
@@ -15,8 +15,16 @@ namespace UNFI.Pages.BasePage {
     public partial class FeedAddPostModalPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button cancelButton;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button postMessageButton;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(FeedAddPostModalPage));
+            cancelButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "cancelButton");
+            postMessageButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "postMessageButton");
         }
     }
 }
