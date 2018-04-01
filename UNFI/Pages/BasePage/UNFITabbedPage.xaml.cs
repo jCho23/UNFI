a488cee6fdcea5bc274f7f8137941b0624323296
June <junecho.ucb@gmail.com>
2018-04-01 11:26:42 -0400

    added UNFI TabbedPage

---
+++ UNFI/Pages/BasePage/UNFITabbedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UNFI.Pages.BasePage
+{
+    public partial class UNFITabbedPage : ContentPage
+    {
+        public UNFITabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
