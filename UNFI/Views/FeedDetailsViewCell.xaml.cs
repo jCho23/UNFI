e50828dabec27ff1aadc4ee0228a2f21cf98da25
June <junecho.ucb@gmail.com>
2018-04-01 11:16:38 -0400

    Added FeedDetailsViewCell

---
+++ UNFI/Views/FeedDetailsViewCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UNFI.Views
+{
+    public partial class FeedDetailsViewCell : ContentPage
+    {
+        public FeedDetailsViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
