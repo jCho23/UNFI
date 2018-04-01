f2bc958dc82c17ae3847e1e06afa760126f69dae
June <junecho.ucb@gmail.com>
2018-04-01 11:19:14 -0400

    Added FeedDetailsViewCell CB

--- UNFI/Views/FeedDetailsViewCell.xaml.cs
+++ UNFI/Views/FeedDetailsViewCell.xaml.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using UNFI.Models;
 using Xamarin.Forms;
 
 namespace UNFI.Views
 {
-    public partial class FeedDetailsViewCell : ContentPage
+    public partial class FeedDetailsViewCell : ViewCell
     {
         public FeedDetailsViewCell()
         {
             InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            var model = BindingContext as ReplyMessageModel;
+
+            ReplyMessageLabel.Text = model.ReplyText;
+            UserProfilePicture.Source = model.ReplyUser.PictureUrl;
         }
     }
 }
