6c93df008bd3bb4b47afa857d877e053b5f4ebc0
June <junecho.ucb@gmail.com>
2018-04-02 16:29:20 -0400

    LB ViewCell

---
+++ UNFI/Views/LeaderboardViewCell.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UNFI.Models;
+using Xamarin.Forms;
+
+namespace UNFI.Views
+{
+    public partial class LeaderboardViewCell : ViewCell
+    {
+        public LeaderboardViewCell()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            var model = BindingContext as ReplyMessageModel;
+
+            ReplyMessageLabel.Text = model.ReplyText;
+            UserProfilePicture.Source = model.ReplyUser.PictureUrl;
+        }
+    }
+}
