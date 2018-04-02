9547ada711aa4582ca34a7e12c8463592904e151
June <junecho.ucb@gmail.com>
2018-04-02 12:39:37 -0400

    added ShiftPage

---
+++ UNFI/Pages/ShiftsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UNFI.Pages
+{
+    public partial class ShiftsPage : ContentPage
+    {
+        public ShiftsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
