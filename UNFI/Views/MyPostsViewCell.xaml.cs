9519566f6aba37a7f447fdfe07af396badb3a4d8
June <junecho.ucb@gmail.com>
2018-04-02 09:42:17 -0400

    CleanUp ViewCell

--- UNFI/Views/MyPostsViewCell.xaml.cs
+++ UNFI/Views/MyPostsViewCell.xaml.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using UNFI.Models;
 using Xamarin.Forms;
 
 namespace UNFI.Views
 {
-    public partial class MyPostsViewCell : ContentPage
+    public partial class MyPostsViewCell : ViewCell
     {
         public MyPostsViewCell()
         {
             InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            var model = BindingContext as PostMessageModel;
+
+            MyPostsMessageLabel.Text = model.PostText;
+            UserProfilePicture.Source = model.PostUser.PictureUrl;
         }
     }
 }
