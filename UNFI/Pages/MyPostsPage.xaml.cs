b0811886e10ecc341e885293a865cf6689b00d9c
June <junecho.ucb@gmail.com>
2018-04-02 09:33:11 -0400

    added MyPostsPage

---
+++ UNFI/Pages/MyPostsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UNFI.Pages
+{
+    public partial class MyPostsPage : ContentPage
+    {
+        public MyPostsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
