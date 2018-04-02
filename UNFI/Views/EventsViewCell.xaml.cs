754f05fb8f1eac3e5ed8cfb4e804266513099f80
June <junecho.ucb@gmail.com>
2018-04-02 14:16:38 -0400

    fin EventsVCCB

--- UNFI/Views/EventsViewCell.xaml.cs
+++ UNFI/Views/EventsViewCell.xaml.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using UNFI.Models;
 using Xamarin.Forms;
 
 namespace UNFI.Views
 {
-    public partial class EventsViewCell : ContentPage
+    public partial class EventsViewCell : ViewCell
     {
         public EventsViewCell()
         {
             InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            var model = BindingContext as PostMessageModel;
+
+            MyPostsMessageLabel.Text = model.PostText;
+            UserProfilePicture.Source = model.PostUser.PictureUrl;
         }
     }
 }
