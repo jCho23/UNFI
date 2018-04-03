e26f99e92f4528ff5fecac0b4c8db1e540a08097
June <junecho.ucb@gmail.com>
2018-04-03 14:17:01 -0400

    Leaderboard ViewCell

--- UNFI/Models/LeaderBoardModel.cs
+++ UNFI/Models/LeaderBoardModel.cs
@@ -3,7 +3,7 @@ namespace UNFI.Models
 {
     public class LeaderBoardModel
     {
-        public string ID { get; set; }
+        public string Rank { get; set; }
         public string TeamName { get; set; }
         public string Points { get; set; }
         public string PictureUrl { get; set; }
