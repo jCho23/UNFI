80faacde16d3023d51545b572d2e2bd59cd8d6df
June <junecho.ucb@gmail.com>
2018-04-03 13:01:58 -0400

    finished LeaderboardModel

--- UNFI/Models/LeaderboardModel.cs
+++ UNFI/Models/LeaderboardModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 namespace UNFI.Models
 {
     public class LeaderboardModel
     {
-        public LeaderboardModel()
-        {
-        }
+        public string ID { get; set; }
+        public string TeamName { get; set; }
+        public string Points { get; set; }
+        public string PictureUrl { get; set; }
     }
 }
