5a000abb6892be0b5268d33523a0bce1f25807af
June <junecho.ucb@gmail.com>
2018-04-08 09:27:31 -0700

    update dummy

--- UNFI/Pages/TipsVideoPage.xaml.cs
+++ UNFI/Pages/TipsVideoPage.xaml.cs
@@ -13,6 +13,7 @@ namespace UNFI.Pages
             tipsVideoListView.ItemsSource = new List<TipsVideoModel>{
                 new TipsVideoModel {Description= "Selector Tips and Tricks", PictureSource = "scan.png"},
                 new TipsVideoModel {Description= "Product Guide Training", PictureSource = "store.png"},
+                new TipsVideoModel {Description= "Scanner Best Practices", PictureSource = "scanner.png"},
 
             };
         }
