0d68ba75c23b7573a9e1bb3ec0e00ae113df9e1a
June <junecho.ucb@gmail.com>
2018-04-08 11:08:43 -0700

    Added ShiftsviewCell

---
+++ UNFI/Views/ShiftsViewCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UNFI.Views
+{
+    public partial class ShiftsViewCell : ContentPage
+    {
+        public ShiftsViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
