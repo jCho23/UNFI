6ac1417ea5d8db45d95cff42e2208e64e556ca74
June <junecho.ucb@gmail.com>
2018-04-08 08:17:23 -0700

    updated image

--- UNFI/Pages/TipsVideoPage.xaml.cs
+++ UNFI/Pages/TipsVideoPage.xaml.cs
@@ -10,12 +10,9 @@ namespace UNFI.Pages
         public TipsVideoPage()
         {
             InitializeComponent();
-            tipsVideoListView.ItemsSource = new List<LeaderBoardModel>{
-                new LeaderBoardModel {Rank = "1", Points= "230", TeamName = "Jordan", PictureUrl = "https://www.pixelstalk.net/wp-content/uploads/2015/12/Air-Jordan-Logo-wallpapers-620x465.jpg"},
-                new LeaderBoardModel {Rank = "2", Points= "200", TeamName = "Jordan", PictureUrl = "https://www.pixelstalk.net/wp-content/uploads/2015/12/Air-Jordan-Logo-wallpapers-620x465.jpg"},
-                new LeaderBoardModel {Rank = "3", Points= "230", TeamName = "Jordan", PictureUrl = "https://www.pixelstalk.net/wp-content/uploads/2015/12/Air-Jordan-Logo-wallpapers-620x465.jpg"},
-                new LeaderBoardModel {Rank = "4", Points= "230", TeamName = "Jordan", PictureUrl = "https://www.pixelstalk.net/wp-content/uploads/2015/12/Air-Jordan-Logo-wallpapers-620x465.jpg"},
-                new LeaderBoardModel {Rank = "5", Points= "230", TeamName = "Jordan", PictureUrl = "https://www.pixelstalk.net/wp-content/uploads/2015/12/Air-Jordan-Logo-wallpapers-620x465.jpg"}
+            tipsVideoListView.ItemsSource = new List<TipsVideoModel>{
+                new TipsVideoModel {Description= "Selector Tips and Tricks to make you more money!", PictureSource = "scan.png"},
+               
 
             };
         }
